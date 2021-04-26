929b5e1c1f71b35bf48d8db6e22e7930ba054c20
Shivang Tripathi <xshivangx@gmail.com>
2021-04-26 16:29:27 +0530

    fix: Move data to component itself

--- pages/Skills/index.tsx
+++ pages/Skills/index.tsx
@@ -1,4 +1,3 @@
-import { skills } from './skillsData';
 import styles from './style.module.scss';
 
 const DEFAULT_SIZE = 50;
@@ -21,3 +20,31 @@ const Skills = () => (
   </ul>
 );
 export default Skills;
+
+const skills = [
+  { name: 'HTML5 / CSS3', src: '/assets/skillsLogos/HTML-CSS.png' },
+  { name: 'Docker', src: '/assets/skillsLogos/docker.png', width: 70 },
+  { name: 'Prettier', src: '/assets/skillsLogos/prettier.png' },
+  { name: 'Git', src: '/assets/skillsLogos/git.png' },
+  {
+    name: 'React Konva',
+    src: '/assets/skillsLogos/react-konva.png',
+  },
+  { name: 'React', src: '/assets/skillsLogos/react.png' },
+  {
+    name: 'React Native',
+    src: '/assets/skillsLogos/reactnative.png',
+    width: 45,
+    height: 75,
+  },
+  { name: 'Redux', src: '/assets/skillsLogos/redux.png' },
+  { name: 'SocketIO', src: '/assets/skillsLogos/socketio.svg' },
+  { name: 'ESLint', src: '/assets/skillsLogos/eslint.png' },
+  { name: 'TS', src: '/assets/skillsLogos/TS.png' },
+  { name: 'Vue', src: '/assets/skillsLogos/vue.png' },
+  {
+    name: 'Webpack',
+    src: '/assets/skillsLogos/webpack.png',
+  },
+  { name: 'Arduino', src: '/assets/skillsLogos/arduino.png' },
+];
